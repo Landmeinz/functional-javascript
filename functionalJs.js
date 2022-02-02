4ecef3c07417e7231632d1f8e75f362a53409615
Eric Meinzer <emeinzer7@gmail.com>
2022-02-02 16:08:15 -0600

    video #1

---
+++ functionalJs.js
@@ -0,0 +1,7 @@
+
+// frontend masters // functional programing via javascript
+
+// WHAT IS FUNCTIONAL PROGRAMMING? // 
+// A coding style supported by some languages
+// a programming paradigm; a world view of how things are controlled
+// 
